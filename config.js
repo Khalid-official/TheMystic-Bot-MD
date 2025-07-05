5e670ab6a368c255fdfd2f26ed10e19b17ac5b7b
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2025-07-06 01:41:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.isBaileysFail = true;
 global.defaultLenguaje = 'es';
 
 global.owner = [
-  ['5219996125657', '👑 Propietario 👑', true],
+  ['5219996125657', '👑 Propietario👑', true],
   ['5492916450307'],
   ['5493794297363'],
   ['59169082575']
